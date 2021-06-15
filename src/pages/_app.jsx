2e25e34c4b040561382fd5dab087fa88af440e32
imad <imadatyatalah@gmail.com>
2021-06-15 08:43:53 +0100

    feat: Added nprogress config

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,12 +1,23 @@
 /* eslint-disable react/prop-types */
 import React from "react";
+import Router from "next/router";
 
 import { ThemeProvider } from "next-themes";
 import { DefaultSeo } from "next-seo";
+import NProgress from "nprogress";
 
 import SEO from "../../next-config.config";
 
+import "nprogress/nprogress.css";
 import "../styles/globals.css";
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+NProgress.configure({ showSpinner: false });
 
 const MyApp = ({ Component, pageProps }) => {
   return (
