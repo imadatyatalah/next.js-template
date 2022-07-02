6ce40f48e58928065112ef8d561dde62a02e817f
Imad Atyat-Allah <imadatyatalah@gmail.com>
2022-07-02 11:07:55 +0100

    chore: update deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,15 @@
 import React from "react";
-import Router from "next/router";
 import type { AppProps } from "next/app";
 
 import { ThemeProvider } from "next-themes";
 import { DefaultSeo } from "next-seo";
-import NProgress from "nprogress";
 
 import SEO from "next-seo.config";
 
-import "nprogress/nprogress.css";
 import "@/styles/globals.css";
-
-Router.events.on("routeChangeStart", () => {
-  NProgress.start();
-});
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
-
-NProgress.configure({ showSpinner: false });
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    // @ts-ignore
     <ThemeProvider defaultTheme="system" attribute="class">
       <DefaultSeo {...SEO} />
       <Component {...pageProps} />
